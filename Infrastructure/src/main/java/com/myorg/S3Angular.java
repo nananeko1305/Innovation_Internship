25202812d8c1a525248f7ec50c344d176de59494
nananeko1305 <nananeko1305@hotmail.com>
2023-05-10 15:03:20 +0200

    Fixed some things

--- Infrastructure/src/main/java/com/myorg/S3Angular.java
+++ Infrastructure/src/main/java/com/myorg/S3Angular.java
@@ -2,9 +2,20 @@ package com.myorg;
 
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
+import software.amazon.awscdk.services.s3.Bucket;
+import software.amazon.awscdk.services.s3.deployment.BucketDeployment;
+import software.amazon.awscdk.services.s3.deployment.ISource;
+import software.amazon.awscdk.services.s3.deployment.Source;
 import software.constructs.Construct;
 
+import java.util.Collections;
+import java.util.List;
+
 public class S3Angular extends Stack {
+
+    String angularPath = "/Front-End/dist/front-end";
+    ISource angularSource = Source.asset(angularPath);
+    List<ISource> sources = Collections.singletonList(angularSource);
 
     public S3Angular(final Construct scope, final String id) {
         this(scope, id, null);
@@ -13,7 +24,16 @@ public class S3Angular extends Stack {
     public S3Angular(final Construct scope, final String id, final StackProps props) {
         super(scope, id, props);
 
-        System.out.println("HelloWorld");
+        Bucket s3BucketAngular = Bucket.Builder
+                .create(this, "S3BucketAngular")
+                .bucketName("s3-bucket-angular")
+                .websiteIndexDocument("index.html").build();
+
+        BucketDeployment.Builder.create(this, "S3BucketDeployment")
+                .sources(sources)
+                .destinationBucket(s3BucketAngular)
+                .build();
+
     }
 
 }
