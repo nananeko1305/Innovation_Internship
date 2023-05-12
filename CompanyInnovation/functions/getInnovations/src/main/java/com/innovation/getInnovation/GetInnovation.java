8f7ce02b12afaef17974c76fa3636c34990e749f
nananeko1305 <nananeko1305@hotmail.com>
2023-05-12 15:25:39 +0200

    Deployed test lambda

---
+++ CompanyInnovation/functions/getInnovations/src/main/java/com/innovation/getInnovation/GetInnovation.java
@@ -0,0 +1,13 @@
+package com.innovation.getInnovation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetInnovation {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GetInnovation.class, args);
+    }
+
+}
