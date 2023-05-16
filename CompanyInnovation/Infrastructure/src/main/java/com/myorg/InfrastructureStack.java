7bb87e92b3546b76c8f25506b48a4c41348a39a8
nananeko1305 <nananeko1305@hotmail.com>
2023-05-16 10:16:46 +0200

    Innovation get start

--- CompanyInnovation/Infrastructure/src/main/java/com/myorg/InfrastructureStack.java
+++ CompanyInnovation/Infrastructure/src/main/java/com/myorg/InfrastructureStack.java
@@ -122,7 +122,7 @@ public class InfrastructureStack extends Stack {
 
         //create lambda to get innovations
        Function getInnovationFunction =
-               Function.Builder.create(this, "handleRequest")
+               Function.Builder.create(this, "GetInnovations")
                 .runtime(Runtime.JAVA_11)
                 .handler("com.innovation.getInnovation.controller.LambdaHandler")
                 .memorySize(512)
