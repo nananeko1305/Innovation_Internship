3cc447a09d9eda2621f92e5a2defe5f04bed3f7c
nananeko1305 <nananeko1305@hotmail.com>
2023-05-17 15:29:29 +0200

    Latest

--- CompanyInnovation/Infrastructure/src/main/java/com/myorg/InfrastructureStack.java
+++ CompanyInnovation/Infrastructure/src/main/java/com/myorg/InfrastructureStack.java
@@ -130,15 +130,15 @@ public class InfrastructureStack extends Stack {
                         .code(Code.fromAsset("../assets/SubmitInnovation.jar"))
                         .build();
 
-        Function manageShopFunction =
-                Function.Builder.create(this, "manageShop")
-                        .runtime(Runtime.JAVA_11)
-                        .handler("com.innovation.manageShop.LambdaHandler")
-                        .memorySize(1024)
-                        .timeout(Duration.seconds(30))
-                        .functionName("lambdaCreate")
-                        .code(Code.fromAsset("../assets/ManageShop.jar"))
-                        .build();
+//        Function manageShopFunction =
+//                Function.Builder.create(this, "manageShop")
+//                        .runtime(Runtime.JAVA_11)
+//                        .handler("com.innovation.manageShop.LambdaHandler")
+//                        .memorySize(1024)
+//                        .timeout(Duration.seconds(30))
+//                        .functionName("manageShop")
+//                        .code(Code.fromAsset("../assets/ManageShop.jar"))
+//                        .build();
 
         Function acceptDeclineFunction =
                 Function.Builder.create(this, "lambdaAcceptDecline")
