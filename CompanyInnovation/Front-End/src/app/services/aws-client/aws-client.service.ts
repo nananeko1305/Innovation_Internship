e4cf31b5a07254b044b87f4f8ead64fb670f1d90
nananeko1305 <nananeko1305@hotmail.com>
2023-05-24 12:39:10 +0200

    Finished shop

--- CompanyInnovation/Front-End/src/app/services/aws-client/aws-client.service.ts
+++ CompanyInnovation/Front-End/src/app/services/aws-client/aws-client.service.ts
@@ -12,30 +12,8 @@ export class AwsClientService {
     let apigClientFactory = require('aws-api-gateway-client').default;
 
     let apigClient = apigClientFactory.newClient({
-      // invokeUrl:'https://7vf89k7mq0.execute-api.eu-north-1.amazonaws.com',
-      invokeUrl:'http://localhost:8080',
-      accessKey: this.storageService.getAccessKey(), //'ACCESS_KEY',
-      secretKey: this.storageService.getSecretKey(), //'SECRET_KEY',
-      sessionToken: this.storageService.getSessionToken(), // 'SESSION_TOKEN', //OPTIONAL: If you are using temporary credentials you must include the session token
-      region: 'eu-north-1' // OPTIONAL: The region where the API is deployed, by default this parameter is set to us-east-1
-    });
-
-    additionalParams = {
-      headers: {
-        jwttoken : this.storageService.getToken()
-      }
-
-    }
-
-    return apigClient.invokeApi({}, receivedPath, receivedMethod, additionalParams, receivedBody)
-  }
-
-  sendRequest2(receivedPath:string, receivedMethod:string, additionalParams:any, receivedBody:any = {}): any {
-    let apigClientFactory = require('aws-api-gateway-client').default;
-
-    let apigClient = apigClientFactory.newClient({
-      // invokeUrl:'https://7vf89k7mq0.execute-api.eu-north-1.amazonaws.com',
-      invokeUrl:'http://localhost:8081',
+      invokeUrl:'https://7vf89k7mq0.execute-api.eu-north-1.amazonaws.com',
+      // invokeUrl:'http://localhost:8080',
       accessKey: this.storageService.getAccessKey(), //'ACCESS_KEY',
       secretKey: this.storageService.getSecretKey(), //'SECRET_KEY',
       sessionToken: this.storageService.getSessionToken(), // 'SESSION_TOKEN', //OPTIONAL: If you are using temporary credentials you must include the session token
